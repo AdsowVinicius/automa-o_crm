bc959be0d060887a1906a5a814593861b11b20d2
Adsow Vinicius <adsow007@gmail.com>
2024-11-25 07:59:50 -0300

    oi

--- app.py
+++ app.py
@@ -4,6 +4,29 @@ import webbrowser
 from time import sleep
 import pyautogui
 
+
+import re
+
+def formatar_telefone(telefone, codigo_pais='+55'):
+    # Remove todos os caracteres que não são números
+    telefone_formatado = re.sub(r'\D', '', telefone)
+    
+    # Verifica se o número tem 11 dígitos e adiciona o código do país
+    if len(telefone_formatado) == 11:
+        telefone_formatado = f'{codigo_pais}{telefone_formatado}'
+    else:
+        raise ValueError("Número de telefone inválido")
+    
+    return telefone_formatado
+
+# Testando a função
+try:
+    telefone = " (12) 98765-4321 "
+    print(formatar_telefone(telefone))  # Saída: +5512987654321
+except ValueError as e:
+    print(e)
+
+
 webbrowser.open('https://web.whatsapp.com/')
 sleep(10)
 teste = openpyxl.load_workbook(r'C:\desenv\python\RPAs(automaçoes)\bot_para_envio_de_mensagens\teste2.xlsx')
@@ -16,6 +39,8 @@ for linha in pagina_teste.iter_rows(min_row=1
     telefone = linha[1].value
     # Caso o número venha como float, converta para int e depois para string
     telefone = str(int(telefone))
+    
+    formatar_telefone(telefone, codigo_pais="55" )
 
     vencimento = linha[2].value
     mensagem = f'olha so {nome} deu certo'
